694f7219793f84a8f83e98277770ef6311235ef5
dustfan <enioy74@gmail.com>
2018-07-08 15:47:36 +0800

    build path modify

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/F2E-5-ComicViewer/dist',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/F2E-3-adminorder/dist/',
+    assetsPublicPath: '/F2E-5-ComicViewer/dist',
 
     /**
      * Source Maps
